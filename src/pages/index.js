dc6f665eab595c3255378d71a4c7c92edc5337b5
Glory Vher Abad <44937979+giddybim@users.noreply.github.com>
2018-12-14 14:50:28 +0800

    Watch buton

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
+    <h1>Learn to design and code React Apps</h1>
+    <p>Complete courses about the best tools and design systems.Prototype and build
+      apps with react and swift.
+    </p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Watch the video</Link>
+  </Layout>
+)
+
+export default IndexPage
